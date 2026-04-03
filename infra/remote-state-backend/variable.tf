77e630e4630f487001a97b7353390b4b08871b4a
mohamedfawzizaki <mohamedfawzizaki@gmail.com>
2026-04-03 17:23:15 +0200

    final-3

---
+++ infra/remote-state-backend/variable.tf
@@ -0,0 +1,9 @@
+variable "remote_state_bucket_name" {
+  description = "Name of the S3 bucket for remote state"
+  type        = string
+}
+
+variable "dynamodb_table_name" {
+  description = "Name of the DynamoDB table for state locking"
+  type        = string
+}
